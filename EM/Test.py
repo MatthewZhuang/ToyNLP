e2a4d091a54d8e61e16d8da0af1e9de5dbc183eb
Matthew <zhuangmianguo@163.com>
2017-10-13 22:05:20 +0800

    test
    test

---
+++ EM/Test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# _*_ coding: utf-8 _*_
+"""
+ @description:
+        
+ @Time       : 17/10/13 下午10:04
+ @Author     : guomianzhuang
+"""
+
+
+if __name__ == '__main__':
+    print("hello world!")
